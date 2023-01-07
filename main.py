8732361ec0ff567a70c7a417c60cbb8fb457d93d
TringaYmeri <94993894+TringaYmeri@users.noreply.github.com>
2023-01-07 12:58:23 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import ftpConnection
+
+# Credentials of the public FTP Server
+FTP_HOST = "ftp.dlptest.com"
+FTP_USER = "dlpuser"
+FTP_PASSWORD = "rNrKYTX9g7z3RgJRmxWuGHbeu" 
+
+folder_path = 'C:\\Users\\HP\\Desktop\\Files'
