084748d5dbcb06cfc2c88034ea85e6f89ac295b8
TeutaMemaj <95043686+TeutaMemaj@users.noreply.github.com>
2023-01-07 14:44:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ FTP_USER = "dlpuser"
 FTP_PASSWORD = "rNrKYTX9g7z3RgJRmxWuGHbeu" 
 
 folder_path = 'C:\\Users\\HP\\Desktop\\Files'
+
+def upload_files(ftp, local_folder):
+    files = os.listdir(local_folder)
+    for file in files:
+        if file.endswith('.doc') or file.endswith('.docx') or file.endswith('.xls') or file.endswith('.xlsx'):
+            with open(local_folder + '/' + file, 'rb') as f:
+                ftp.storbinary('STOR ' + file, f)
