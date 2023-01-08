ee931a62b988f5122c113f775e6b67951ea442d4
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2023-01-08 14:17:14 +0100

    Formatimi dhe optimizimi i kodit
    Ky ndryshim eshte verzioni final i kodit te aplikacionit tone. Kam reduktuar disa fajlla te panevojshem dhe kam shtuar disa komente, ne menyre qe kodi te jete sa me i kuptueshem.

--- ftpConnection.py
+++
@@ -1,6 +0,0 @@
-import ftplib
-
-def ftp_connection(ftp_host, ftp_username, ftp_password):
-    ftp = ftplib.FTP(ftp_host)
-    ftp.login(ftp_username, ftp_password)
-    return ftp
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
-import ftpConnection
+import ftplib
 
-# Credentials of the public FTP Server
+# Credentials for the public FTP Server
 FTP_HOST = "ftp.dlptest.com"
 FTP_USER = "dlpuser"
-FTP_PASSWORD = "rNrKYTX9g7z3RgJRmxWuGHbeu" 
+FTP_PASSWORD = "rNrKYTX9g7z3RgJRmxWuGHbeu"  # it may change depending on the public FTP server
 
 folder_path = 'C:\\Users\\HP\\Desktop\\Files'
 
@@ -14,11 +14,17 @@ def upload_files(ftp, local_folder):
         if file.endswith('.doc') or file.endswith('.docx') or file.endswith('.xls') or file.endswith('.xlsx'):
             with open(local_folder + '/' + file, 'rb') as f:
                 ftp.storbinary('STOR ' + file, f)
+            print(file + " uploaded successfully!")    
                 
-                ftp = ftpConnection.connect_ftp(FTP_HOST, FTP_USER, FTP_PASSWORD)
 
+# connect to FTP server
+ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASSWORD)
+
+# upload files from local folder to FTP server
 upload_files(ftp, folder_path)
 
+# we can use ftp.dir() if we want to see our documents in realtime in FTP server
 ftp.dir() 
 
+# close FTP connection
 ftp.quit()
